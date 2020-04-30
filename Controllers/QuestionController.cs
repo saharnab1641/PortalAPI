95fc312e4ae6818d6c7ed2c08f5eacaa85070ee9
Arnab Saha <saharnab1641@gmail.com>
2020-04-30 12:23:52 +0530

    Added support for transcripts

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -12,6 +12,14 @@ using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using PortalAPI.Models;
+using iText.Forms;
+using iText.Kernel.Pdf;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage.Auth;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace PortalAPI.Controllers
 {
@@ -19,7 +27,7 @@ namespace PortalAPI.Controllers
     [ApiController]
     public class QuestionController : ControllerBase
     {
-        private IConfiguration _config;
+        private readonly IConfiguration _config;
         public QuestionController(IConfiguration config)
         {
             _config = config;
@@ -27,6 +35,10 @@ namespace PortalAPI.Controllers
 
         private static readonly string EndpointUri = "https://codesizzlercsdb.documents.azure.com:443";
         private static readonly string PrimaryKey = "gJZB3746gfqEVhwL5uFj85k28iPdbAc4prX3W9vxbSs9ChQmSy2DGtwp9GDw79UA5kMFLSQ9AFPs4TLDwCM2oQ==";
+        private static readonly string AccountKey = "zdUViL51nXIZ5ILevk1zFpCqFv6qaf8wsH6sIwMvbPPMGdGHFYM37s3/7zMhQ4VE8xoAmTeAO5RlS70jKu8+4A==";
+        private static readonly string AccountName = "examportaldocs";
+        private static readonly string TemplateSrc = "https://examportaldocs.blob.core.windows.net/template/certificatetemplate.pdf";
+
         private CosmosClient cosmosClient;
         private Database Qdatabase;
         private Container Qcontainer;
@@ -39,17 +51,17 @@ namespace PortalAPI.Controllers
 
 
         // The name of the database and container we will create
-        private string QdatabaseId = "QuestionDB";
-        private string QcontainerId = "QuestionContainer";
+        private readonly string QdatabaseId = "QuestionDB";
+        private readonly string QcontainerId = "QuestionContainer";
 
-        private string UdatabaseId = "RegUsersDB";
-        private string UcontainerId = "RegUsersContainer";
+        private readonly string UdatabaseId = "RegUsersDB";
+        private readonly string UcontainerId = "RegUsersContainer";
 
-        private string RdatabaseId = "ReportDB";
-        private string RcontainerId = "ReportContainer";
+        private readonly string RdatabaseId = "ReportDB";
+        private readonly string RcontainerId = "ReportContainer";
 
-        private string EdatabaseId = "ExamDB";
-        private string EcontainerId = "ExamContainer";
+        private readonly string EdatabaseId = "ExamDB";
+        private readonly string EcontainerId = "ExamContainer";
 
         [HttpPost("AddExam")]
         [EnableCors("AllPolicy")]
@@ -89,6 +101,12 @@ namespace PortalAPI.Controllers
         [Authorize]
         public async Task<IActionResult> GetEnrolled()
         {
+            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            Udatabase = await CreateDatabaseAsync(UdatabaseId);
+            Ucontainer = await CreateContainerAsync(Udatabase, UcontainerId, "id");
+            Udatabase = await CreateDatabaseAsync(UdatabaseId);
+            Ucontainer = await CreateContainerAsync(Udatabase, UcontainerId, "id");
+
             var currentUser = HttpContext.User;
             var userId = currentUser.Claims.FirstOrDefault(c => c.Type == "id").Value;
 
@@ -96,6 +114,32 @@ namespace PortalAPI.Controllers
 
             return Ok(new { enrolled = UserResponse.Resource.Enrolled });
         }
+
+        [HttpGet("GetReports")]
+        [EnableCors("AllPolicy")]
+        [Authorize]
+        public async Task<IActionResult> GetReports()
+        {
+            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            Udatabase = await CreateDatabaseAsync(UdatabaseId);
+            Ucontainer = await CreateContainerAsync(Udatabase, UcontainerId, "id");
+            Rdatabase = await CreateDatabaseAsync(RdatabaseId);
+            Rcontainer = await CreateContainerAsync(Rdatabase, RcontainerId, "id");
+
+            var currentUser = HttpContext.User;
+            var userId = currentUser.Claims.FirstOrDefault(c => c.Type == "id").Value;
+
+            ItemResponse<UserModel> UserResponse = await Ucontainer.ReadItemAsync<UserModel>(userId, new PartitionKey(userId));
+            List<ReportModel> reports = new List<ReportModel>();
+            foreach (string itemToRead in UserResponse.Resource.Reports)
+            {
+                ItemResponse<ReportModel> TransResponse = await Rcontainer.ReadItemAsync<ReportModel>(itemToRead, new PartitionKey(itemToRead));
+                reports.Add(TransResponse.Resource);
+            }
+
+            return Ok(new { reports });
+        }
+
 
         [HttpPost("AddQuestion")]
         [EnableCors("AllPolicy")]
@@ -174,6 +218,7 @@ namespace PortalAPI.Controllers
                 await Econtainer.ReadItemAsync<ExamModel>(exam, new PartitionKey(exam));
                 UserResponse = await Ucontainer.ReadItemAsync<UserModel>(userId, new PartitionKey(userId));
                 if (!UserResponse.Resource.Enrolled.ContainsKey(exam)) throw new Exception("User not enrolled in exam.");
+                if (UserResponse.Resource.Enrolled[exam]) throw new Exception("Exam already given.");
             }
             catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
@@ -200,8 +245,40 @@ namespace PortalAPI.Controllers
             {
 
             }*/
+
             cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            Udatabase = await CreateDatabaseAsync(UdatabaseId);
+            Ucontainer = await CreateContainerAsync(Udatabase, UcontainerId, "id");
+            Edatabase = await CreateDatabaseAsync(EdatabaseId);
+            Econtainer = await CreateContainerAsync(Edatabase, EcontainerId, "id");
+            Rdatabase = await CreateDatabaseAsync(RdatabaseId);
+            Rcontainer = await CreateContainerAsync(Rdatabase, RcontainerId, "id");
+            ItemResponse<ExamModel> ExamResponse;
+
+            try
+            {
+                ExamResponse = await Econtainer.ReadItemAsync<ExamModel>(examID, new PartitionKey(examID));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = "No such exam found." });
+            }
+
+
             var currentUser = HttpContext.User;
+            var userId = currentUser.Claims.FirstOrDefault(c => c.Type == "id").Value;
+
+            ItemResponse<UserModel> UserResponse = await Ucontainer.ReadItemAsync<UserModel>(userId, new PartitionKey(userId));
+
+            if (!UserResponse.Resource.Enrolled.ContainsKey(examID))
+            {
+                return BadRequest(new { error = "User does not seem to be enrolled in course." });
+            }
+            if (UserResponse.Resource.Enrolled[examID])
+            {
+                return BadRequest(new { error = "User has already given the exam." });
+            }
+
 
             int correctWeight = 1;
             int wrongWeight = 0;
@@ -243,6 +320,8 @@ namespace PortalAPI.Controllers
                                     {
                                         score += correctWeight;
                                     }
+                                    else score -= wrongWeight;
+
                                     break;
                                 }
                             }
@@ -317,23 +396,6 @@ namespace PortalAPI.Controllers
                 }
             }
 
-            Udatabase = await CreateDatabaseAsync(UdatabaseId);
-            Ucontainer = await CreateContainerAsync(Udatabase, UcontainerId, "id");
-
-            Edatabase = await CreateDatabaseAsync(EdatabaseId);
-            Econtainer = await CreateContainerAsync(Edatabase, UcontainerId, "id");
-
-            ItemResponse<ExamModel> ExamResponse = await Econtainer.ReadItemAsync<ExamModel>(examID, new PartitionKey(examID));
-
-            var userId = currentUser.Claims.FirstOrDefault(c => c.Type == "id").Value;
-
-            ItemResponse<UserModel> UserResponse = await Ucontainer.ReadItemAsync<UserModel>(userId, new PartitionKey(userId));
-
-            if (!UserResponse.Resource.Enrolled.ContainsKey(examID))
-            {
-                return BadRequest(new { error = "User does not seem to be enrolled in course." });
-            }
-
             ReportModel report = new ReportModel
             {
                 Id = Guid.NewGuid().ToString(),
@@ -348,11 +410,6 @@ namespace PortalAPI.Controllers
             UserResponse.Resource.Reports.Add(report.Id);
             UserResponse.Resource.Enrolled[examID] = true;
 
-            //Save report
-
-            Rdatabase = await CreateDatabaseAsync(RdatabaseId);
-            Rcontainer = await CreateContainerAsync(Rdatabase, RcontainerId, "id");
-
             try
             {
                 // Read the item to see if it exists.  
@@ -360,15 +417,45 @@ namespace PortalAPI.Controllers
             }
             catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
+                string fileName = "test" + GenerateRandomString(5, "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") + "_" + buildDateTimeStamp() + ".pdf";
+                MemoryStream writeStream = new MemoryStream();
+                PdfWriter pdfWriter = new PdfWriter(writeStream);
+                pdfWriter.SetCloseStream(false);
+                PdfDocument pdfDoc = new PdfDocument(new PdfReader(TemplateSrc), pdfWriter);
+                PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+
+                form.GetField("name").SetValue(UserResponse.Resource.Name).SetReadOnly(true);
+                form.GetField("email").SetValue(UserResponse.Resource.Id).SetReadOnly(true);
+                form.GetField("address").SetValue(UserResponse.Resource.Address).SetReadOnly(true);
+                form.GetField("exam").SetValue(ExamResponse.Resource.ExamType + " - " + ExamResponse.Resource.ExamSerial).SetReadOnly(true);
+                form.GetField("dateeval").SetValue(report.PassingDate).SetReadOnly(true);
+                form.GetField("score").SetValue(score.ToString()).SetReadOnly(true);
+
+
+                pdfDoc.Close();
+
+                StorageCredentials storageCredentials = new StorageCredentials(AccountName, AccountKey);
+                CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("certificates");
+                await container.CreateIfNotExistsAsync();
+                await container.SetPermissionsAsync(new BlobContainerPermissions()
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+                CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+                blob.Properties.ContentType = "application/pdf";
+                writeStream.Position = 0;
+                await blob.UploadFromStreamAsync(writeStream);
+                string html = "Dear User,<br><br>Your exam has been successfully conducted. The transcript has been attached with this mail.";
+                Email(html, UserResponse.Resource.Id, "Exam Report", writeStream);
+
+                var blobUri = blob.Uri.ToString();
+                report.ReportURL = blobUri;
+                writeStream.Close();
 
                 ItemResponse<ReportModel> UserReport = await Rcontainer.CreateItemAsync(report, new PartitionKey(report.Id));
                 await Ucontainer.ReplaceItemAsync(UserResponse.Resource, UserResponse.Resource.Id, new PartitionKey(UserResponse.Resource.Id));
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", UserReport.Resource.Id, UserReport.RequestCharge);
-                string htmlMail = "Dear User,<br><br>This is a detailed report for the exam you have taken recently.<br><br>" +
-                "<b>Exam:<b> " + ExamResponse.Resource.ExamType + "-" + ExamResponse.Resource.ExamSerial +
-                "<br><b>Passing Date:<b> " + report.PassingDate +
-                "<br><b>Score:<b> " + report.Scored;
-                Email(htmlMail, report.Email);
             }
             //Return report json
 
@@ -376,6 +463,63 @@ namespace PortalAPI.Controllers
         }
 
         //----------------------------Helper Methods----------------------------//
+
+        private string GenerateRandomString(int length, string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?=_-")
+        {
+            if (length < 0) throw new ArgumentOutOfRangeException("length", "length cannot be less than zero.");
+            if (string.IsNullOrEmpty(allowedChars)) throw new ArgumentException("allowedChars may not be empty.");
+
+            const int byteSize = 0x100;
+            var allowedCharSet = new HashSet<char>(allowedChars).ToArray();
+            if (byteSize < allowedCharSet.Length) throw new ArgumentException(string.Format("allowedChars may contain no more than {0} characters.", byteSize));
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var result = new StringBuilder();
+                var buf = new byte[128];
+                while (result.Length < length)
+                {
+                    rng.GetBytes(buf);
+                    for (var i = 0; i < buf.Length && result.Length < length; ++i)
+                    {
+                        var outOfRangeStart = byteSize - (byteSize % allowedCharSet.Length);
+                        if (outOfRangeStart <= buf[i]) continue;
+                        result.Append(allowedCharSet[buf[i] % allowedCharSet.Length]);
+                    }
+                }
+                return result.ToString();
+            }
+        }
+
+        private string buildDateTimeStamp()
+
+        {
+            StringBuilder sb = new StringBuilder();
+            DateTime currentDate = DateTime.Now;
+            sb.Append(currentDate.Year.ToString());
+            if (currentDate.Month.ToString().Length == 1)
+                sb.Append("0" + currentDate.Month.ToString());
+            else
+                sb.Append(currentDate.Month.ToString());
+            if (currentDate.Day.ToString().Length == 1)
+                sb.Append("0" + currentDate.Day.ToString());
+            else
+                sb.Append(currentDate.Day.ToString());
+            if (currentDate.Hour.ToString().Length == 1)
+                sb.Append("0" + currentDate.Hour.ToString());
+            else
+                sb.Append(currentDate.Hour.ToString());
+            if (currentDate.Minute.ToString().Length == 1)
+                sb.Append("0" + currentDate.Minute.ToString());
+            else
+                sb.Append(currentDate.Minute.ToString());
+            if (currentDate.Second.ToString().Length == 1)
+
+                sb.Append("0" + currentDate.Second.ToString());
+            else
+                sb.Append(currentDate.Second.ToString());
+            return sb.ToString();
+        }
 
         private async Task<Database> CreateDatabaseAsync(string dbId)
         {
@@ -406,7 +550,7 @@ namespace PortalAPI.Controllers
             return "Added";
         }
 
-        public static void Email(string htmlString, string email)
+        public static void Email(string htmlString, string email, string subject, MemoryStream ms)
         {
             try
             {
@@ -414,9 +558,14 @@ namespace PortalAPI.Controllers
                 SmtpClient smtp = new SmtpClient();
                 message.From = new MailAddress("saharnab.test@gmail.com");
                 message.To.Add(new MailAddress(email));
-                message.Subject = "Exam Report";
+                message.Subject = subject;
                 message.IsBodyHtml = true; //to make message body as html  
                 message.Body = htmlString;
+                if (ms != null)
+                {
+                    ms.Position = 0;
+                    message.Attachments.Add(new Attachment(ms, "Test.pdf", "application/pdf"));
+                }
                 smtp.Port = 587;
                 smtp.Host = "smtp.gmail.com"; //for gmail host  
                 smtp.EnableSsl = true;
@@ -446,7 +595,6 @@ namespace PortalAPI.Controllers
             while (resultSet.HasMoreResults)
             {
                 FeedResponse<dynamic> response = await resultSet.ReadNextAsync();
-                dynamic question = response.First();
                 if (response.Diagnostics != null)
                 {
                     Console.WriteLine($" Diagnostics {response.Diagnostics.ToString()}");
